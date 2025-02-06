37328b0990ae4d5bf12044dc65b5e60f6db0ac5f
omkar454 <omraut41105@gmail.com>
2025-02-06 18:24:35 +0530

    Update DataBase_Credentials.java

--- DataBase_Credentials.java
+++ DataBase_Credentials.java
@@ -1,8 +1,8 @@
 public class DataBase_Credentials {
-
-     String JDBC_url = "jdbc:mysql://localhost:3306/ExamHub";
-     String username = "root";
-     String password = "OmRaut@04";
+# Use your actual DATABASE credentials for succesful JDBC connection
+     String JDBC_url = "jdbc:mysql://localhost:3306/YOUR_DATABASE_NAME";
+     String username = "YOUR_USERNAME";
+     String password = "YOUR_PASSWORD";
 
 
 }
